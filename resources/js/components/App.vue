3a1a007179fdbbe64aea29cabdce104c3b91f550
VadimMalets <vadim1996xc@gmail.com>
2019-07-12 12:15:59 +0300

    Main page, components added.

---
+++ resources/js/components/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="app">
+        <house-component/>
+    </div>
+</template>
+
+<script>
+    import HouseComponent from "./HouseComponent.vue"
+
+    export default {
+        name: "App",
+        components: {
+            HouseComponent
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
